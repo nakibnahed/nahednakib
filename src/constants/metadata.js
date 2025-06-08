2dbb66b4a9708877a5a94bfec58ee0d38847f61b
nakibnahed <nahednakibyos@gmail.com>
2025-06-08 12:04:54 +0200

    meta data share

--- src/constants/metadata.js
+++ src/constants/metadata.js
@@ -2,11 +2,11 @@
 export const metadata = {
   title: "Nahed | Web Development",
   description:
-    "Nahed Nakib - A professional web developer and distance runner. Explore my portfolio and journey.",
+    "Nahed Nakib - Web developer and distance runner. Explore my portfolio and journey.",
   keywords: "Nahed Nakib, web developer, portfolio, running, programming",
   author: "Nahed Nakib",
   url: "https://nahednakib.vercel.app",
-  image: "https://nahednakib.vercel.app/images/me.jpg",
+  image: "https://nahednakib.vercel.app/images/share.jpg",
   icons: {
     icon: "/favicon.ico",
   },
