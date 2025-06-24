da7e93b3dcf5e9ca44066265cc785a4791435ace
nakibnahed <nahednakibyos@gmail.com>
2025-06-24 15:46:10 +0200

    social links

--- src/components/SocialLinks/socialLinksData.js
+++ src/components/SocialLinks/socialLinksData.js
@@ -1,9 +1,17 @@
 // components/SocialLinks/socialLinksData.js
 
 export const socialLinks = [
-  { id: 1, name: "facebook", link: "https://facebook.com/yourprofile" },
+  {
+    id: 1,
+    name: "facebook",
+    link: "https://www.facebook.com/profile.php?id=100069454789973",
+  },
   { id: 2, name: "instagram", link: "https://instagram.com/nahed_nakib" },
-  { id: 3, name: "linkedin", link: "https://linkedin.com/in/yourprofile" },
-  { id: 4, name: "twitter", link: "https://twitter.com/yourprofile" },
-  { id: 5, name: "github", link: "https://github.com/yourprofile" },
+  {
+    id: 3,
+    name: "linkedin",
+    link: "https://www.linkedin.com/in/nahed-nakib-450ab9239/",
+  },
+  { id: 4, name: "twitter", link: "https://x.com/nahednakib?s=21" },
+  { id: 5, name: "github", link: "https://github.com/nakibnahed" },
 ];
