8a11f025bfd302175541e699fe4f3014cbc7e25c
nakibnahed <nahednakibyos@gmail.com>
2026-04-02 11:09:57 +0200

    seo edits

--- src/app/sitemap.js
+++ src/app/sitemap.js
@@ -1,9 +1,35 @@
-export const revalidate = 3600;
-
 import { createAnonPublicClient } from "@/lib/supabase/anon-public";
 import { getSiteUrl } from "@/lib/seo/site";
 
-export default async function sitemap() {
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
+
+function escapeXml(value) {
+  return String(value)
+    .replace(/&/g, "&amp;")
+    .replace(/</g, "&lt;")
+    .replace(/>/g, "&gt;")
+    .replace(/"/g, "&quot;")
+    .replace(/'/g, "&apos;");
+}
+
+function buildUrlNode({ url, lastModified, changeFrequency, priority }) {
+  const lastMod =
+    lastModified instanceof Date
+      ? lastModified.toISOString()
+      : new Date(lastModified).toISOString();
+
+  return [
+    "<url>",
+    `<loc>${escapeXml(url)}</loc>`,
+    `<lastmod>${escapeXml(lastMod)}</lastmod>`,
+    `<changefreq>${escapeXml(changeFrequency)}</changefreq>`,
+    `<priority>${priority}</priority>`,
+    "</url>",
+  ].join("");
+}
+
+export async function GET() {
   const baseUrl = getSiteUrl();
   const now = new Date();
 
@@ -68,5 +94,18 @@ export default async function sitemap() {
     console.error("sitemap: Supabase fetch failed", e);
   }
 
-  return [...staticPages, ...blogPages, ...portfolioPages];
+  const pages = [...staticPages, ...blogPages, ...portfolioPages];
+
+  const xml = [
+    '<?xml version="1.0" encoding="UTF-8"?>',
+    '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">',
+    ...pages.map(buildUrlNode),
+    "</urlset>",
+  ].join("");
+
+  return new Response(xml, {
+    headers: {
+      "Content-Type": "application/xml",
+    },
+  });
 }
