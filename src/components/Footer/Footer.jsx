8ba32df347d88d3e8ef3866af9bbc14a37fcfa78
nakibnahed <nahednakibyos@gmail.com>
2026-05-29 16:57:53 +0200

    new edits in footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -6,7 +6,7 @@ export default function Footer() {
   return (
     <div className={styles.container}>
       <div>
-        <p>©2026 Nahed. All Right Reserved.</p>
+        <p>©{new Date().getFullYear()} Nahed. All Rights Reserved.</p>
       </div>
       <div className={styles.social}>
         <SocialLinks />
